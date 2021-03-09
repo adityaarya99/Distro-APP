04f17f1022ff63203d36945c419d233e1df0403e
adityaarya99 <aryaadityacr7@gmail.com>
2021-03-09 16:54:25 +0530

    Images updated Recycler View

--- Distro/app/src/main/java/com/example/distro/ProgrammingAdapter.java
+++ Distro/app/src/main/java/com/example/distro/ProgrammingAdapter.java
@@ -10,13 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 import java.util.zip.Inflater;
 
 public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.ProgrammingViewHolder> {
     private String[] data;
+    List<Integer> imageList;
+    TechData techData;
 
-    public ProgrammingAdapter(String[] data) {
+    public ProgrammingAdapter(TechData techData, String[] data, List<Integer> imageList) {
+        this.techData=techData;
         this.data = data;
+        this.imageList=imageList;
     }
 
     @NonNull
@@ -31,6 +38,8 @@ public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.
     public void onBindViewHolder(@NonNull ProgrammingViewHolder holder, int position) {
         String title = data[position];
         holder.texttitle.setText(title);
+        Picasso.with(techData).load(imageList.get(position)).into(holder.imgicon);
+
     }
 
     @Override
--- Distro/app/src/main/java/com/example/distro/TechData.java
+++ Distro/app/src/main/java/com/example/distro/TechData.java
@@ -6,15 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class TechData extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+ public class TechData extends AppCompatActivity {
+    List<Integer> ImageList = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tech_data);
-        RecyclerView oslist = (RecyclerView) findViewById(R.id.oslist);
+        RecyclerView oslist = (RecyclerView) findViewById(R.id.osystemlist);
         oslist.setLayoutManager(new LinearLayoutManager(this));
+        ImageList.add(R.drawable.windows);
+        ImageList.add(R.drawable.nontech);
+        ImageList.add(R.drawable.logo);
+        ImageList.add(R.drawable.nontech);
+        ImageList.add(R.drawable.windows);
         String[] operatingsystem = { "Windows","MacOS","Linux","Ubuntu" };
-        oslist.setAdapter(new ProgrammingAdapter(operatingsystem));
+        oslist.setAdapter(new ProgrammingAdapter(this,operatingsystem,ImageList));
     }
 }
